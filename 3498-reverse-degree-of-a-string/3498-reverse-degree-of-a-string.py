361bb4568bc24ebdf8201a11315fdc9c40b87ff3
Dev-Satyam29 <gupta29satyam@gmail.com>
2026-09-20 15:50:19 +0530

    Time: 5 ms (81.22%), Space: 19.2 MB (55.57%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,9 +1,9 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        freq= {chr(i): 27 - (i - 96) for i in range(97, 123)}
         total=0
         for i in range(len(s)):
-            product=freq[s[i]]*(i+1)
+            n=27-(ord(s[i])-96)
+            product=n*(i+1)
             total+=product
         return total
         
